2045549495b64ff3df1b29b10096ef62eb69e7c0
marinesnow34 <m4rinesnow34@gmail.com>
2023-11-15 10:59:42 +0900

    Config: 예외 처리 코드 추가

---
+++ src/main/java/com/readyvery/readyverydemo/global/exception/ExceptionCode.java
@@ -0,0 +1,16 @@
+package com.readyvery.readyverydemo.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ExceptionCode {
+	STORE_NOT_FOUND(404, "Store does not exists.");
+
+	private int status;
+	private String message;
+
+	ExceptionCode(int status, String message) {
+		this.status = status;
+		this.message = message;
+	}
+}
