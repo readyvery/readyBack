0c6035944aec3fa00058ff1bd5dddf54e926aec1
1223v <1223v@naver.com>
2023-11-17 20:22:48 +0900

    Feat: 로그아웃 API 기능 추가. RT만 삭제 -> AT는 클라이언트에서 삭제

--- src/main/java/com/readyvery/readyverydemo/src/user/UserServiceImpl.java
+++ src/main/java/com/readyvery/readyverydemo/src/user/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.readyvery.readyverydemo.src.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +13,8 @@ import com.readyvery.readyverydemo.src.user.dto.UserAuthRes;
 import com.readyvery.readyverydemo.src.user.dto.UserInfoRes;
 import com.readyvery.readyverydemo.src.user.dto.UserMapper;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -21,6 +24,8 @@ public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
 	private final UserMapper userMapper;
+	@Value("${jwt.refresh.cookie}")
+	private String refreshCookie;
 
 	@Override
 	public UserAuthRes getUserAuthById(CustomUserDetails userDetails) {
@@ -33,6 +38,26 @@ public class UserServiceImpl implements UserService {
 	public UserInfoRes getUserInfoById(Long id) {
 		UserInfo userInfo = getUserInfo(id);
 		return userMapper.userInfoToUserInfoRes(userInfo);
+	}
+
+	@Override
+	public void removeRefreshTokenInDB(Long id, HttpServletResponse response) {
+		UserInfo user = getUserInfo(id);
+		user.updateRefresh(null); // Refresh Token을 null 또는 빈 문자열로 업데이트
+		userRepository.save(user);
+		invalidateRefreshTokenCookie(response); // 쿠키 무효화
+	}
+
+	/**
+	 * 로그아웃
+	 * @param response
+	 */
+	private void invalidateRefreshTokenCookie(HttpServletResponse response) {
+		Cookie refreshTokenCookie = new Cookie(refreshCookie, null); // 쿠키 이름을 동일하게 설정
+		refreshTokenCookie.setHttpOnly(true);
+		refreshTokenCookie.setPath("/api/v1/refresh/token"); // 기존과 동일한 경로 설정
+		refreshTokenCookie.setMaxAge(0); // 만료 시간을 0으로 설정하여 즉시 만료
+		response.addCookie(refreshTokenCookie);
 	}
 
 	private UserInfo getUserInfo(Long id) {
