6e5d01cde221d1c92bc28337b607cb0ca76e25aa
marinesnow34 <m4rinesnow34@gmail.com>
2023-11-25 01:43:13 +0900

    Feat: coupon domain 수정

--- src/main/java/com/readyvery/readyverydemo/domain/Event.java
+++ src/main/java/com/readyvery/readyverydemo/domain/Event.java
@@ -1,15 +1,10 @@
 package com.readyvery.readyverydemo.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,10 +26,6 @@ public class Event extends BaseTimeEntity {
 	@Column(name = "event_idx")
 	private Long id;
 
-	// 이벤트 베너 이미지 url
-	@Column(name = "banner_img")
-	private String bannerImg;
-
 	// 이벤트 메인 이미지 url
 	@Column(name = "main_img")
 	private String mainImg;
@@ -42,9 +33,4 @@ public class Event extends BaseTimeEntity {
 	// 이벤트 진행 여부
 	@Column(name = "is_active")
 	private boolean isActive;
-
-	@Builder.Default
-	@OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
-	private List<CouponDetail> coupons = new ArrayList<CouponDetail>();
-
 }
