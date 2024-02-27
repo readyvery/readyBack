ea7175c4df56ce2b6100006b3ee887c818c5e25f
Joowon Lim <m4rinesnow34@gmail.com>
2024-02-27 14:32:48 +0900

    Fix: 포인트 오류 수정

--- src/main/java/com/readyvery/readyverydemo/domain/Point.java
+++ src/main/java/com/readyvery/readyverydemo/domain/Point.java
@@ -11,12 +11,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "point")
 public class Point extends BaseTimeEntity {
 	@Id
