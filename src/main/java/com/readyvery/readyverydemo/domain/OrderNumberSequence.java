2d84c830146fa522a839f27a9b31a13c4ff202f8
Joowon Lim <m4rinesnow34@gmail.com>
2024-03-27 03:19:09 +0900

    Feat: 주문번호순서 테이블 생성

---
+++ src/main/java/com/readyvery/readyverydemo/domain/OrderNumberSequence.java
@@ -0,0 +1,24 @@
+package com.readyvery.readyverydemo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class OrderNumberSequence {
+	@Id
+	private Long id;
+
+	@Column(name = "order_number")
+	private Long orderNumber;
+
+	@OneToOne
+	@MapsId
+	@JoinColumn(name = "id") // Order 테이블과 조인하는 컬럼 지정
+	private Store store;
+}
