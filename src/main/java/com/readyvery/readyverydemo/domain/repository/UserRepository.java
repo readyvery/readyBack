2ab03967c2aa83693c8250658e6a784207ff22b6
1223v <1223v@naver.com>
2025-07-30 20:57:50 +0900

    Fix: User 인덱싱 적용

--- src/main/java/com/readyvery/readyverydemo/domain/repository/UserRepository.java
+++ src/main/java/com/readyvery/readyverydemo/domain/repository/UserRepository.java
@@ -16,8 +16,6 @@ public interface UserRepository extends JpaRepository<UserInfo, Long> {
 
 	Optional<UserInfo> findByEmail(String email);
 
-	Optional<UserInfo> findByRefreshToken(String refreshToken);
-
 	/**
 	 * 소셜 타입과 소셜의 식별값으로 회원 찾는 메소드
 	 * 정보 제공을 동의한 순간 DB에 저장해야하지만, 아직 추가 정보(사는 도시, 나이 등)를 입력받지 않았으므로
